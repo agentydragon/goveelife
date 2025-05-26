5f2131ce49376df2789d9dfe1dae6fb98bc8ed24
Rai <agentydragon@gmail.com>
2025-05-26 11:51:35 -0700

    wip

--- custom_components/goveelife/__init__.py
+++ custom_components/goveelife/__init__.py
@@ -37,31 +37,32 @@ _LOGGER: Final = logging.getLogger(__name__)
 
 async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     """Set up cloud resource from the config entry."""
+    prefix = f"{entry.entry_id} - async_setup_entry: "
     _LOGGER.debug("Setting up config entry: %s", entry.entry_id)
 
     try:
-        _LOGGER.debug("%s - async_setup_entry: Creating data store: %s.%s ", entry.entry_id, DOMAIN, entry.entry_id)
+        _LOGGER.debug(f"{prefix}Creating data store: {DOMAIN}.{entry.entry_id}")
         hass.data.setdefault(DOMAIN, {})
         hass.data[DOMAIN].setdefault(entry.entry_id, {})
         entry_data = hass.data[DOMAIN][entry.entry_id]
         entry_data[CONF_PARAMS] = entry.data
         entry_data[CONF_SCAN_INTERVAL] = None
     except Exception:
-        _LOGGER.error("%s - async_setup_entry: Creating data store failed", entry.entry_id)
+        _LOGGER.error(f"{prefix}Creating data store failed")
         return False
 
     try:
-        _LOGGER.debug("%s - async_setup_entry: Receiving cloud devices..", entry.entry_id)
+        _LOGGER.debug(f"{prefix}Receiving cloud devices..")
         api_devices = await async_GoveeAPI_GETRequest(hass, entry.entry_id, 'user/devices')
         if api_devices is None:
             return False
         entry_data[CONF_DEVICES] = api_devices
     except Exception:
-        _LOGGER.error("%s - async_setup_entry: Receiving cloud devices failed", entry.entry_id)
+        _LOGGER.error(f"{prefix}Receiving cloud devices failed")
         return False 
 
     try:
-        _LOGGER.debug("%s - async_setup_entry: Creating update coordinators per device..", entry.entry_id)
+        _LOGGER.debug(f"{prefix}Creating update coordinators per device..")
         entry_data.setdefault(CONF_COORDINATORS, {})
         for device_cfg in api_devices:
             await async_GoveeAPI_GetDeviceState(hass, entry.entry_id, device_cfg)
@@ -69,30 +70,30 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
             d = device_cfg.get('device')
             entry_data[CONF_COORDINATORS][d] = coordinator            
     except Exception:
-        _LOGGER.error("%s - async_setup_entry: Creating update coordinators failed", entry.entry_id)
+        _LOGGER.error(f"{prefix}Creating update coordinators failed")
         return False 
 
     try:
-        _LOGGER.debug("%s - async_setup_entry: Register option updates listener: %s ", entry.entry_id, FUNC_OPTION_UPDATES)
+        _LOGGER.debug(f"{prefix}Register option updates listener: {FUNC_OPTION_UPDATES}")
         entry_data[FUNC_OPTION_UPDATES] = entry.add_update_listener(options_update_listener)
     except Exception:
-        _LOGGER.error("%s - async_setup_entry: Register option updates listener failed", entry.entry_id)
+        _LOGGER.error(f"{prefix}Register option updates listener failed")
         return False
 
     try:
         await hass.config_entries.async_forward_entry_setups(entry, SUPPORTED_PLATFORMS)
     except Exception:
-        _LOGGER.error("%s - async_setup_entry: Setup trigger for platform failed", entry.entry_id)
+        _LOGGER.error(f"{prefix}Setup trigger for platform failed")
         return False
 
     try:
-        _LOGGER.debug("%s - async_setup_entry: register services", entry.entry_id)
+        _LOGGER.debug(f"{prefix}register services")
         await async_registerService(hass, "set_poll_interval", async_service_SetPollInterval)
     except Exception:
-        _LOGGER.error("%s - async_setup_entry: register services failed", entry.entry_id)
+        _LOGGER.error(f"{prefix}register services failed")
         return False 
 
-    _LOGGER.debug("%s - async_setup_entry: Completed", entry.entry_id)
+    _LOGGER.debug(f"{prefix}Completed")
     return True
 
 async def options_update_listener(hass: HomeAssistant, entry: ConfigEntry) -> bool:
@@ -102,16 +103,17 @@ async def options_update_listener(hass: HomeAssistant, entry: ConfigEntry) -> bo
 
 async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     """Unload a config entry."""
+    prefix = f"{entry.entry_id} - async_unload_entry: "
     try:
         _LOGGER.debug("Unloading config entry: %s", entry.entry_id)
         all_ok = True
 
         # Unload platforms
         for platform in SUPPORTED_PLATFORMS:
-            _LOGGER.debug("%s - async_unload_entry: unload platform: %s", entry.entry_id, platform)
+            _LOGGER.debug(f"{prefix}unload platform: {platform}")
             platform_ok = await hass.config_entries.async_forward_entry_unload(entry, platform)
             if not platform_ok:
-                _LOGGER.error("%s - async_unload_entry: failed to unload: %s (%s)", entry.entry_id, platform, platform_ok)
+                _LOGGER.error(f"{prefix}failed to unload: {platform} ({platform_ok})")
                 all_ok = platform_ok
 
         if all_ok:
@@ -119,18 +121,18 @@ async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
             entity_registry = hass.helpers.entity_registry.async_get()
             entities = hass.helpers.entity_registry.async_entries_for_config_entry(entity_registry, entry.entry_id)
             for entity in entities:
-                _LOGGER.debug("%s - async_unload_entry: removing entity: %s", entry.entry_id, entity.entity_id)
+                _LOGGER.debug(f"{prefix}removing entity: {entity.entity_id}")
                 entity_registry.async_remove(entity.entity_id)
 
             # Unload option updates listener
-            _LOGGER.debug("%s - async_unload_entry: Unload option updates listener: %s.%s ", entry.entry_id, FUNC_OPTION_UPDATES)
+            _LOGGER.debug(f"{prefix}Unload option updates listener: {FUNC_OPTION_UPDATES}")
             hass.data[DOMAIN][entry.entry_id][FUNC_OPTION_UPDATES]()
 
             # Remove data store
-            _LOGGER.debug("%s - async_unload_entry: Remove data store: %s.%s ", entry.entry_id, DOMAIN, entry.entry_id)
+            _LOGGER.debug(f"{prefix}Remove data store: {DOMAIN}.{entry.entry_id}")
             hass.data[DOMAIN].pop(entry.entry_id)
 
         return all_ok
     except Exception:
-        _LOGGER.error("%s - async_unload_entry: Unload device failed", entry.entry_id)
+        _LOGGER.error(f"{prefix}Unload device failed")
         return False
